2071cc36d0c4e5aad5e036dbeaa8360bf9d9ce37
Hattou Faïssal <faissal.hattou@gmail.com>
2023-06-11 18:09:32 +0200

    Setup environment file & config

---
+++ src/config/index.ts
@@ -0,0 +1,7 @@
+const config = {
+  TMDB_API_BASE_URL: process.env.TMDB_API_BASE_URL,
+  TMDB_IMAGE_BASE_URL: process.env.TMDB_IMAGE_BASE_URL,
+  TMDB_API_KEY: process.env.TMDB_API_KEY,
+};
+
+export default config;
