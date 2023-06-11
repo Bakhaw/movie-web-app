ebf24ddf9c359bed04de51f745236bc38ea164eb
Hattou Faïssal <faissal.hattou@gmail.com>
2023-06-11 18:53:14 +0200

    Initialize color palette in Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      black: "#000000",
+      grey: "#56535E",
+      orange: "#C66E2F",
+      purple: "#312438",
+      "purple-light": "#4B3953",
+      "purple-dark": "#2D2032",
+      "purple-grey": "#2C1F31",
+      red: "#E80016",
+      white: "#ffffff",
     },
   },
   plugins: [],
-}
+};
