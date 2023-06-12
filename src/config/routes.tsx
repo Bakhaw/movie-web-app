689c3fe419fd5ac1aae7b616c583dac236c21924
Hattou Faïssal <faissal.hattou@gmail.com>
2023-06-12 05:08:26 +0200

    Moved routes inside config folder

---
+++ src/config/routes.tsx
@@ -0,0 +1,23 @@
+import { NavigationLinkProps } from "@/components/NavigationLink";
+
+import IconMovie from "@/icons/IconMovie";
+import IconTrendingUp from "@/icons/IconTrendingUp";
+import IconTv from "@/icons/IconTv";
+
+export const routes: NavigationLinkProps[] = [
+  {
+    href: "/",
+    icon: <IconTrendingUp />,
+    text: "Trending",
+  },
+  {
+    href: "/movies",
+    icon: <IconMovie />,
+    text: "Movies",
+  },
+  {
+    href: "/tv",
+    icon: <IconTv />,
+    text: "TV",
+  },
+];
