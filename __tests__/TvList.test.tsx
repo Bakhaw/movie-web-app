4a439c59d2a97b6d0b6c2179086f72d42a5ad63b
Faïssal <fhattou.ext@unowhy.com>
2023-06-12 22:29:07 +0200

    Add tests for MovieList & TvList

--- __tests__/TvList.test.tsx
+++ __tests__/TvList.test.tsx
@@ -3,10 +3,6 @@ import { render, screen } from "@testing-library/react";
 
 import TvList from "@/components/TvList";
 
-jest.mock("next/router", () => ({
-  useRouter: jest.fn(),
-}));
-
 const mockTv = [
   { id: 1, name: "TV Show 1", poster_path: "/path/to/poster1.jpg" },
   { id: 2, name: "TV Show 2", poster_path: "/path/to/poster2.jpg" },
