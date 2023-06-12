d948ad0fe7b41a7a8afc2ae72d1883f22ee0efe2
Hattou Faïssal <faissal.hattou@gmail.com>
2023-06-12 05:05:51 +0200

    Add icons: movie, trending-up & tv

---
+++ src/icons/IconTrendingUp.tsx
@@ -0,0 +1,21 @@
+function IconTrendingUp(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      viewBox="0 0 24 24"
+      height="1em"
+      width="1em"
+      {...props}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" />
+      <path d="M3 17l6-6 4 4 8-8" />
+      <path d="M14 7h7v7" />
+    </svg>
+  );
+}
+
+export default IconTrendingUp;
