5e85807461aa7f7293aebf72884c3b4d30dbb948
Faïssal <fhattou.ext@unowhy.com>
2023-06-12 22:27:45 +0200

    Remove unused keys from Movie & TV types

--- src/types/index.ts
+++ src/types/index.ts
@@ -8,70 +8,29 @@ interface Genre {
   name: string;
 }
 
-interface Language {
-  english_name: string;
-  iso_639_1: string;
-  name: string;
-}
-
 export interface Movie {
-  adult: boolean;
-  backdrop_path: string;
-  belongs_to_collection?: {
-    id: string;
-    name: string;
-    poster_path: string;
-    backdrop_path: string;
-  };
-  budget: number;
-  genres: Genre[];
-  homepage: string;
+  backdrop_path?: string;
+  genres?: Genre[];
   id: number;
-  imdb_id: string;
-  original_language: string;
-  original_title: string;
-  overview: string;
-  popularity: number;
+  overview?: string;
+  popularity?: number;
   poster_path: string;
-  production_companies: ProductionCompany[];
-  production_countries: ProductionCountry[];
-  release_date: string;
-  revenue: number;
-  runtime: number;
-  spoken_languages: Language[];
-  status: string;
-  tagline: string;
+  release_date?: string;
   title: string;
-  video: boolean;
-  vote_average: number;
-  vote_count: number;
-}
-
-interface ProductionCompany {
-  id: number;
-  logo_path: string;
-  name: string;
-  origin_country: string;
-}
-
-interface ProductionCountry {
-  iso_3166_1: string;
-  name: string;
+  video?: boolean;
+  vote_average?: number;
+  vote_count?: number;
 }
 
 export interface TV {
-  adult: boolean;
-  backdrop_path: string;
-  first_air_date: string;
-  id: string;
+  backdrop_path?: string;
+  id: number;
   name: string;
-  original_language: string;
-  original_name: string;
-  overview: string;
+  overview?: string;
   poster_path: string;
-  popularity: number;
-  vote_average: number;
-  vote_count: number;
+  popularity?: number;
+  vote_average?: number;
+  vote_count?: number;
 }
 
 export enum DataSourceType {
