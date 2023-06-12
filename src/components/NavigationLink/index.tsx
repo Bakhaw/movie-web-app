64a9d93cbc34f48a836032e4c03aa22a4fff63af
Hattou Faïssal <faissal.hattou@gmail.com>
2023-06-12 05:08:06 +0200

    Moved NavigationLink to its own folder

--- src/components/NavigationLink/index.tsx
+++ src/components/NavigationLink/index.tsx
@@ -5,11 +5,13 @@ import { usePathname } from "next/navigation";
 import classNames from "classnames";
 
 export interface NavigationLinkProps {
+  children?: React.ReactNode;
   href: string;
-  text: string;
+  icon?: React.ReactNode;
+  text?: string;
 }
 
-const NavigationLink: React.FC<NavigationLinkProps> = ({ href, text }) => {
+const NavigationLink: React.FC<NavigationLinkProps> = ({ children, href }) => {
   const pathname = usePathname();
   const isActive = pathname === href;
 
@@ -17,12 +19,13 @@ const NavigationLink: React.FC<NavigationLinkProps> = ({ href, text }) => {
     <Link
       href={href}
       className={classNames(
-        "block w-full text-sm md:text-base capitalize",
-        isActive && "font-bold  ml-4 border-r-4 border-orange",
+        "block w-full text-sm md:text-base capitalize hover:text-orange",
+        isActive &&
+          "font-bold  ml-4 border-t-4 md:border-r-4 md:border-t-0 border-orange",
         isActive ? "text-orange" : "text-white" // ternary here otherwise it's not possible to swap out for the right color due to CSS "colors" declaration order
       )}
     >
-      {text}
+      {children}
     </Link>
   );
 };
