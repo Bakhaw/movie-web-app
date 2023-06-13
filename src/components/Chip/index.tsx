020beee3bca7483a6cc37e77f720942348e9b757
Faïssal <fhattou.ext@unowhy.com>
2023-06-13 02:01:39 +0200

    Add clamp on Chip component

--- src/components/Chip/index.tsx
+++ src/components/Chip/index.tsx
@@ -5,7 +5,9 @@ interface ChipProps {
 }
 
 const Chip: React.FC<ChipProps> = ({ children }) => (
-  <div className="px-4 py-[1px] bg-grey rounded-2xl text-sm">{children}</div>
+  <div className="px-4 py-[1px] bg-grey rounded-2xl text-sm shrink-0 line-clamp-1">
+    {children}
+  </div>
 );
 
 export default Chip;
