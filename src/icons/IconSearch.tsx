42606ba5b58582ee82862b525828c52b37b709e0
Hattou Faïssal <faissal.hattou@gmail.com>
2023-06-15 14:51:45 +0200

    Add IconSearch

---
+++ src/icons/IconSearch.tsx
@@ -0,0 +1,20 @@
+function IconSearch(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      viewBox="0 0 24 24"
+      height="1em"
+      width="1em"
+      {...props}
+    >
+      <path d="M19 11 A8 8 0 0 1 11 19 A8 8 0 0 1 3 11 A8 8 0 0 1 19 11 z" />
+      <path d="M21 21l-4.35-4.35" />
+    </svg>
+  );
+}
+
+export default IconSearch;
