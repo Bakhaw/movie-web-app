6d25f5518c9526584dd6edd260f27d1a2748dba9
Hattou Faïssal <faissal.hattou@gmail.com>
2023-06-15 11:56:16 +0200

    Update env

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 const config = {
-  TMDB_API_BASE_URL: process.env.TMDB_API_BASE_URL,
-  TMDB_IMAGE_BASE_URL: process.env.TMDB_IMAGE_BASE_URL,
-  TMDB_API_KEY: process.env.TMDB_API_KEY,
+  TMDB_API_BASE_URL: process.env.NEXT_PUBLIC_TMDB_API_BASE_URL,
+  TMDB_IMAGE_BASE_URL: process.env.NEXT_PUBLIC_TMDB_IMAGE_BASE_URL,
+  TMDB_API_KEY: process.env.NEXT_PUBLIC_TMDB_API_KEY,
 };
 
 export default config;
