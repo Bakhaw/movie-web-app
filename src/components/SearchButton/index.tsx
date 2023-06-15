47cd2e102558ae14d28291f3e4159352a460ffa6
Hattou Faïssal <faissal.hattou@gmail.com>
2023-06-15 14:53:15 +0200

    Add SearchButton & SearchForm components

---
+++ src/components/SearchButton/index.tsx
@@ -0,0 +1,9 @@
+import IconSearch from "@/icons/IconSearch";
+
+const SearchButton = () => (
+  <button className="h-10 px-4 bg-purple-light text-white">
+    <IconSearch />
+  </button>
+);
+
+export default SearchButton;
---
+++ src/components/SearchForm/index.tsx
@@ -0,0 +1,38 @@
+import { FormEvent, RefObject } from "react";
+
+import IconClose from "@/icons/IconClose";
+import SearchButton from "../SearchButton";
+
+interface SearchFormProps {
+  onClear: () => void;
+  onSubmit: (e: FormEvent<HTMLFormElement>) => void;
+  inputRef: RefObject<HTMLInputElement>;
+}
+
+const SearchForm: React.FC<SearchFormProps> = ({
+  onClear,
+  onSubmit,
+  inputRef,
+}) => (
+  <form
+    className="flex flex-wrap justify-center items-center sm:justify-start gap-2"
+    onSubmit={onSubmit}
+  >
+    <div className="flex justify-between items-center border-grey border-[1px]">
+      <input
+        className="h-10 w-full md:w-[260px] px-4 text-white bg-purple rounded-sm outline-none"
+        ref={inputRef}
+        placeholder="Search"
+        type="text"
+      />
+
+      <button type="button">
+        <IconClose className="text-sm text-white mr-2" onClick={onClear} />
+      </button>
+    </div>
+
+    <SearchButton />
+  </form>
+);
+
+export default SearchForm;
