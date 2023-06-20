5e99533d7f66600136e24967dfe60e270dbee094
Hattou Faïssal <faissal.hattou@gmail.com>
2023-06-21 01:35:28 +0200

    Remove local Select & SearchButton components

--- src/components/SearchButton/index.tsx
+++
@@ -1,9 +0,0 @@
-import IconSearch from "@/icons/IconSearch";
-
-const SearchButton = () => (
-  <button className="h-10 px-4 bg-purple-light text-white">
-    <IconSearch />
-  </button>
-);
-
-export default SearchButton;
