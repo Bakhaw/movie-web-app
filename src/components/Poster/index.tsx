01a2d68266416a0a62ea4d99fdd01941a9c1e7b2
Hattou Faïssal <faissal.hattou@gmail.com>
2023-07-05 05:34:58 +0200

    Add framer-motion animation on Poster

--- src/components/Poster/index.tsx
+++ src/components/Poster/index.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Image from "next/image";
+import { AnimatePresence, motion } from "framer-motion";
 
 import { cn } from "@/lib/utils";
 
@@ -17,32 +20,38 @@ const Poster: React.FC<PosterProps> = ({
   title,
   width,
   height,
-  ...props
 }) => (
-  <div className={cn("space-y-3", className)} {...props}>
-    <div className="overflow-hidden rounded-md">
-      <Image
-        alt={`${title} poster`}
-        className={cn(
-          "h-auto w-auto object-cover transition-all hover:scale-105 aspect-[2/3]",
-          className
-        )}
-        src={src}
-        width={width}
-        height={height}
-      />
-    </div>
-
-    {(title || subtitle) && (
-      <div className="space-y-1 text-sm">
-        {title && <h3 className="font-medium leading-none">{title}</h3>}
-
-        {subtitle && (
-          <p className="text-xs text-muted-foreground">{subtitle}</p>
-        )}
+  <AnimatePresence>
+    <motion.div
+      className={cn("space-y-3", className)}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
+      <div className="overflow-hidden rounded-md">
+        <Image
+          alt={`${title} poster`}
+          className={cn(
+            "h-auto w-auto object-cover transition-all hover:scale-105 aspect-[2/3]",
+            className
+          )}
+          src={src}
+          width={width}
+          height={height}
+        />
       </div>
-    )}
-  </div>
+
+      {(title || subtitle) && (
+        <div className="space-y-1 text-sm">
+          {title && <h3 className="font-medium leading-none">{title}</h3>}
+
+          {subtitle && (
+            <p className="text-xs text-muted-foreground">{subtitle}</p>
+          )}
+        </div>
+      )}
+    </motion.div>
+  </AnimatePresence>
 );
 
 export default Poster;
